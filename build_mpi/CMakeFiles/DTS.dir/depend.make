c4a1ae00231366a376d1d5b4768e368115661e69
adriarwin <adriarwin@gmail.com>
2024-09-09 09:57:34 +0200

    this

--- build_mpi/CMakeFiles/DTS.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DTS.
-# This may be replaced when dependencies are built.
--- build_single/CMakeFiles/DTS.dir/dts_src/ParallelTemperingDistributedMemory.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DTS.dir/dts_src/ParallelTemperingDistributedMemory.cpp.o: \
- /mnt/c/Users/Usuario/phd/FreeDTS2/dts_src/ParallelTemperingDistributedMemory.cpp \
- /usr/include/stdc-predef.h
